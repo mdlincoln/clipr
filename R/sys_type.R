f8123b9f07e77930b08bdc92eac54886466659ae
Matthew Lincoln <matthew.d.lincoln@gmail.com>
2015-08-27 13:56:05 -0400

    Remove redundant comments

--- R/sys_type.R
+++ R/sys_type.R
@@ -1,7 +1,4 @@
-#' Determine system type
+# Determine system type
 sys_type <- function() {
   return(Sys.info()["sysname"])
-  # OS X will return "Darwin"
-  # Ubuntu will return "Linux"
-  # Windows will return "Windows" (?)
 }
