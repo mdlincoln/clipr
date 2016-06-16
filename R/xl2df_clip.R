76b1a244e2186dcc7ae7c130700a850f128bba38
Steve Simpson <steventroysimpson@gmail.com>
2016-06-16 10:16:08 -0400

    added xl2df_clip for parsing clips into dataframes

---
+++ R/xl2df_clip.R
@@ -0,0 +1,44 @@
+#' Transforms Output of Read_clip into Data.Frame
+#'
+#' Transforms Output of Read_clip into Data.Frame
+#'
+#' @param clip the excel table clip to transform to data.frame
+#' @param headers logical for if clip contains colnames or a vector of colnames to use
+#' @param stringsAsFactor logical for using stringAsFactors, default is FALSE
+#' @export
+#' @examples
+#' # excel contents
+#' #' #            thingOne        thingTwo
+#' # 1 the thing I might the thing I may
+#' # 2    become despite coding this way
+#'
+#' # excel contents after read_clip copies
+#' clip_contents_from_excel_table <- c(
+#' "thingOne\tthingTwo",
+#' "the thing I might\tthe thing I may",
+#' "become despite\tcoding this way")
+#'
+#' cat_in_the_hat <- xl2df_clip(clip_contents_from_excel_table )
+#' print(cat_in_the_hat)
+#' #            thingOne        thingTwo
+#' # 1 the thing I might the thing I may
+#' # 2    become despite coding this way
+xl2df_clip <- function (clip = clipr::read_clip(), headers = TRUE, stringsAsFactor = FALSE) {
+    out <- t(sapply(clip, function(x) strsplit(x, "\t")[[1]],
+        USE.NAMES = FALSE))
+    if (is.logical(headers)) {
+        if (headers) {
+            xx <- out[2:nrow(out), ]
+            colnames(xx) <- out[1, ]
+        }
+        else {
+            xx <- out
+        }
+    }
+    else {
+        xx <- out[2:nrow(out), ]
+        colnames(xx) <- headers
+    }
+    as.data.frame(xx, stringsAsFactors = stringsAsFactor)
+}
+
