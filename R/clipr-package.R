3971cc7676fdf79976b97a540b84d6b44dea2ee5
Matthew Lincoln <matthew.d.lincoln@gmail.com>
2016-11-13 21:27:21 -0800

    package-level docs

---
+++ R/clipr-package.R
@@ -0,0 +1,16 @@
+#' clipr: Read and Write from the System Clipboard
+#'
+#' Simple utility functions to read from and write to the Windows, OS X, and X11
+#' clipboards.
+#'
+#' The basic functions \code{\link{read_clip}} and \code{\link{write_clip}} wrap
+#' platform-specific functions for writing values from R to the system
+#' clipboard. \code{\link{read_clip_tbl}} will attempt to process the clipboard
+#' content like a table copied from a spreadsheet program.
+#'
+#' \code{\link{is_clipboard_available}} is useful when buildling packages that
+#' depend on \link{clipr} functionality.
+#'
+#' @docType package
+#' @name clipr
+NULL
