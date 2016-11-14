df76755c6414e1234fc72c8bfb3a0b8280d2acf3
Matthew Lincoln <matthew.d.lincoln@gmail.com>
2016-11-13 19:40:50 -0800

    addin to capture R console output, rather than expression value

--- R/clipr_addin.R
+++ R/clipr_addin.R
@@ -12,3 +12,18 @@ clipr_addin <- function() {
   expr_object <- eval(parse(text = context$selection[[1]]$text))
   write_clip(expr_object)
 }
+
+#' RStudio addin
+#'
+#' Copies the console output (rather than the value) of a selected expression to
+#' the system clipboard
+#'
+#' @note Requires the
+#'   \href{https://cran.r-project.org/package=rstudioapi}{rstudioapi} package.
+#'
+#' @export
+clipr_output_addin <- function() {
+  context <- rstudioapi::getActiveDocumentContext()
+  expr_object <- eval(parse(text = context$selection[[1]]$text))
+  write_clip(capture.output(expr_object))
+}
