28999e1caf32cd2fa315489948a78afa382ba790
Matthew Lincoln <matthew.d.lincoln@gmail.com>
2016-11-13 20:08:44 -0800

    rstudio addins for clipping expr results AND console output

--- R/clipr_addin.R
+++ R/clipr_addin.R
@@ -1,28 +1,10 @@
-#' RStudio addin
-#'
-#' Write the results of the selected expression to the
-#' clipboard
-#'
-#' @note Requires the \href{https://cran.r-project.org/package=rstudioapi}{rstudioapi}
-#' package.
-#'
-#' @export
-clipr_addin <- function() {
+clipr_result <- function() {
   context <- rstudioapi::getActiveDocumentContext()
   expr_object <- eval(parse(text = context$selection[[1]]$text))
   write_clip(expr_object)
 }
 
-#' RStudio addin
-#'
-#' Copies the console output (rather than the value) of a selected expression to
-#' the system clipboard
-#'
-#' @note Requires the
-#'   \href{https://cran.r-project.org/package=rstudioapi}{rstudioapi} package.
-#'
-#' @export
-clipr_output_addin <- function() {
+clipr_output <- function() {
   context <- rstudioapi::getActiveDocumentContext()
   expr_object <- eval(parse(text = context$selection[[1]]$text))
   write_clip(capture.output(expr_object))
