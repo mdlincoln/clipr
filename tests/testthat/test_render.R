edba93b424473b146c778a5c861870859f085da4
Matthew Lincoln <matthew.d.lincoln@gmail.com>
2016-11-24 23:36:04 -0800

    try just writing to the clipboard first

--- tests/testthat/test_render.R
+++ tests/testthat/test_render.R
@@ -6,8 +6,13 @@ test_that("clipr_available fails when DISPLAY is not configured; succeeds when i
   skip_if_not(identical(Sys.getenv("TRAVIS"), "true"))
   if (identical(Sys.getenv("DISPLAY"), ""))
     expect_false(clipr_available())
-  if (identical(Sys.getenv("DISPLAY"), ":90.0"))
+  if (identical(Sys.getenv("DISPLAY"), ":90.0")) {
+    expect_equivalent({
+      write_clip("a")
+      read_clip()
+    }, "a")
     expect_true(clipr_available())
+  }
 })
 
 test_that("Unavailable clipboard throws warning", {
