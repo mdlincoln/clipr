0427cab15014647a4368eca2c161717482da203c
Matthew Lincoln <matthew.d.lincoln@gmail.com>
2018-06-13 22:40:30 -0700

    unit tests for has_xclip/has_xsel

---
+++ tests/testthat/test-systems.R
@@ -0,0 +1,24 @@
+context("systems")
+
+test_that("utility checking works on Linux-like", {
+
+  if (identical(Sys.getenv("TRAVIS_CLIP"), "xclip")) {
+    expect_true(has_xclip())
+    expect_false(has_xsel())
+  }
+
+  if (identical(Sys.getenv("TRAVIS_CLIP"), "xsel")) {
+    expect_false(has_xclip())
+    expect_true(has_xsel())
+  }
+
+  if (identical(Sys.getenv("TRAVIS_CLIP"), "none")) {
+    expect_false(has_xclip())
+    expect_false(has_xsel())
+  }
+
+  if (identical(Sys.getenv("TRAVIS_CLIP"), "nodisplay")) {
+    expect_false(has_xclip())
+    expect_false(has_xsel())
+  }
+})
