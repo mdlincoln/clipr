6042bb5fc44a7cf07fac18538e30ce7bb42f31ed
Matthew Lincoln <matthew.d.lincoln@gmail.com>
2018-06-13 22:30:40 -0700

    break meta-clipr tests into separate test file
    Also moves setup functions that chek for clipr availability into setup.R which is run first when test() is called.

---
+++ tests/testthat/setup.R
@@ -0,0 +1,3 @@
+# Setup for all following tests
+skip_msg <- "System clipboard is not available - skipping test."
+is_clipr_available <- clipr_available()
---
+++ tests/testthat/test-diagnostics.R
@@ -0,0 +1,36 @@
+context("diagnostics")
+
+test_that("clipr_available fails when DISPLAY is not configured; succeeds when it is", {
+  # Only run this test on Travis
+  skip_if_not(identical(Sys.getenv("TRAVIS"), "true"))
+  if (identical(Sys.getenv("TRAVIS_CLIP"), "none"))
+    expect_false(is_clipr_available)
+  if (identical(Sys.getenv("TRAVIS_CLIP"), "xclip"))
+    expect_true(is_clipr_available)
+  if (identical(Sys.getenv("TRAVIS_CLIP"), "xsel"))
+    expect_true(is_clipr_available)
+})
+
+test_that("dr_clipr provides informative messages", {
+  if (identical(Sys.getenv("TRAVIS_CLIP"), "xclip"))
+    expect_message(dr_clipr(), msg_clipr_available(), fixed = TRUE)
+  if (identical(Sys.getenv("TRAVIS_CLIP"), "xsel"))
+    expect_message(dr_clipr(), msg_clipr_available(), fixed = TRUE)
+  if (identical(Sys.getenv("TRAVIS_CLIP"), "none"))
+    expect_message(dr_clipr(), msg_no_clipboard(), fixed = TRUE)
+  if (identical(Sys.getenv("TRAVIS_CLIP"), "nodisplay"))
+    expect_message(dr_clipr(), msg_no_display(), fixed = TRUE)
+})
+
+test_that("Unavailable clipboard throws warning", {
+  if (!is_clipr_available) {
+    expect_error(write_clip("a"))
+  }
+})
+
+test_that("clipr_available() does not overwrite existing contents", {
+  skip_if_not(is_clipr_available, skip_msg)
+  write_clip("z")
+  clipr_available()
+  expect_equal(read_clip(), "z")
+})
