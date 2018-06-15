59eb34680ddcab8ce98684b3140f8874f9d406d4
Matthew Lincoln <matthew.d.lincoln@gmail.com>
2018-06-14 22:01:37 -0700

    the NODISPLAY travis test DOES have xclip installed

--- tests/testthat/test-systems.R
+++ tests/testthat/test-systems.R
@@ -18,7 +18,7 @@ test_that("utility checking works on Linux-like", {
   }
 
   if (identical(Sys.getenv("TRAVIS_CLIP"), "nodisplay")) {
-    expect_false(has_xclip())
+    expect_true(has_xclip())
     expect_false(has_xsel())
   }
 })
