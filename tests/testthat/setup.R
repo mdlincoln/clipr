ee5fcb891b802127c4f91b46326d181eab3fee8b
Matthew Lincoln <matthew.d.lincoln@gmail.com>
2019-01-10 15:30:33 -0500

    try explicitly setting in .travis.yml

--- tests/testthat/setup.R
+++ tests/testthat/setup.R
@@ -1,9 +1,9 @@
 # Setup for all following tests
 skip_msg <- "System clipboard is not available - skipping test."
 # If not running on cran, set the global envvar that allows clipr to run in non-interactive session
-if (Sys.getenv("NOT_CRAN") == "TRUE") {
-  message("Not on cran, allowing interactive clipr")
-  Sys.setenv(ALLOW_CLIPBOARD=TRUE)
-}
+# if (Sys.getenv("NOT_CRAN") == "TRUE") {
+#   message("Not on cran, allowing interactive clipr")
+#   Sys.setenv(ALLOW_CLIPBOARD="TRUE")
+# }
 is_clipr_available <- clipr_available()
 message("Is clipr available?: ", is_clipr_available)
